c2285a4783186abd5b819dbbbc0f4288224de0b3
qijieguan <qijieguan7@gmail.com>
2022-02-07 20:59:17 -0800

    updated portfolio

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,6 +5,7 @@ import shop_img from './images/shop.jpeg';
 import schedule_img from './images/schedule.jpg';
 import group_img from './images/group.jpeg';
 import food_img from './images/food.jpg';
+import chat_img from './images/chat.jpeg';
 
 const Home = () => {
 
@@ -20,13 +21,9 @@ const Home = () => {
             <div className="projects">
                 <h1 style={fontStyle2}>Projects</h1>
                 <ul className="project-ul">
-                    <li className="project-li" onClick={() => window.open("https://cryptic-beyond-98060.herokuapp.com/")}>
-                        <img className='project-li-img' src={recipe_img} alt=""></img>
-                        <div className='project-li-name'>Recipe App</div>
-                    </li>
-                    <li className="project-li" onClick={() => window.open("https://protected-meadow-96612.herokuapp.com/")}>
-                        <img className='project-li-img' src={movie_img} alt=""></img>
-                        <div className='project-li-name'>Movie App</div>
+                <li className="project-li" onClick={() => window.open("https://still-peak-27529.herokuapp.com/")}>
+                        <img className='project-li-img' src={chat_img} alt=""></img>
+                        <div className='project-li-name'>Chat App</div>
                     </li>
                     <li className="project-li" onClick={() => window.open("https://desolate-thicket-47298.herokuapp.com/")}>
                         <img className='project-li-img' src={shop_img} alt=""></img>
@@ -39,6 +36,14 @@ const Home = () => {
                     <li className="project-li" onClick={() => window.open("https://secret-hamlet-60850.herokuapp.com/")}>
                         <img className='project-li-img' src={group_img} alt=""></img>
                         <div className='project-li-name'>Q&A App</div>
+                    </li>
+                    <li className="project-li" onClick={() => window.open("https://protected-meadow-96612.herokuapp.com/")}>
+                        <img className='project-li-img' src={movie_img} alt=""></img>
+                        <div className='project-li-name'>Movie App</div>
+                    </li>
+                    <li className="project-li" onClick={() => window.open("https://cryptic-beyond-98060.herokuapp.com/")}>
+                        <img className='project-li-img' src={recipe_img} alt=""></img>
+                        <div className='project-li-name'>Recipe App</div>
                     </li>
                 </ul>
                 <h1 style={fontStyle2}>To Be Deployed</h1>
